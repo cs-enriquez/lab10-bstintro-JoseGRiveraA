dd5b4d6e8699c0f3ba73053194bac0b6dd480b7f
JoseGRiveraA <147435761+JoseGRiveraA@users.noreply.github.com>
2024-11-24 21:38:06 +0000

    reatetree omplete

--- BSTLab1.cpp
+++ BSTLab1.cpp
@@ -8,11 +8,21 @@ class Node {
 		// the Node should contain
 		// 	- an integer "key" that holds the value of the Node
 		// 	- a pointer to each child possible child node ("right" and "left")
+		int key;
+		Node *right;
+		Node *left; 
 
 
 		// Write a constructor for the Node
 		// input: only an integer value for "key", no pointer arguments
 		// 		right and left pointers should be null
+
+		Node(int k)
+		{
+			key = k;
+			right = nullptr;
+			left = nullptr;
+		}
 
 };
 
@@ -21,23 +31,34 @@ class Node {
 // {13, 0, 7, 6, 21, 15, -2, 12, 99, 18, 19, -1}
 Node* createTree() {
 	// root
-	Node *root = nullptr;
+	Node *root = new Node(13);
 
 	// level 1 (children of root)
+	root -> left = new Node(0);
+	root -> right = new Node(21);
 
 	// level 2 (children of 0)
-
+	root -> left -> left = new Node(-2);
+	root -> left -> right = new Node(7);
+;
 	// level 2 (children of 21)
+	root -> right -> left = new Node(15);
+	root -> right -> right = new Node(99);
 
 	// level 3 (children of -2)
+	root -> left -> left -> right = new Node(-1);
 
 	// level 3 (children of 7)
+	root -> left -> right -> left = new Node(6);
+	root -> left -> right -> right = new Node(12);
 
 	// level 3 (children of 15)
+	root -> right -> left -> right = new Node(18);
 
 	// level 3 (children of 99)
 
 	// level 4 (children of 18)
+	root -> right -> left -> right -> right = new Node(19);
 
 	return root;
 }
